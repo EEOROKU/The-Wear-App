2037be2a96de16bcd59b05cf7a400db641e3cb59
EEOROKU <113193015+EEOROKU@users.noreply.github.com>
2024-03-22 04:50:38 -0300

    Homepage, done-ish

--- lib/screens/home_page_etc.dart
+++ lib/screens/home_page_etc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,24 +12,121 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(
-                'https://as1.ftcdn.net/v2/jpg/03/39/45/96/1000_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg'),
+  static List<Widget> _widgetOptions = <Widget>[
+    Column(children: [
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundImage: NetworkImage(
+                  'https://as1.ftcdn.net/v2/jpg/03/39/45/96/1000_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg'),
+            ),
           ),
-        ),
-        Text(
-          'Username',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-        ),
-        Text('Bio'),
-      ],
-    ),
+          Column(children: [
+            Text(
+              'Username',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            Text('See public profile'),
+          ]),
+        ],
+      ),
+      Row(
+        children: [Icon(Icons.location_pin), Text('Charlottetown'), Text('                                         OOTD calendar>',style: TextStyle(color: Colors.blue),)],
+      ),
+      Row(
+        children: [
+          SizedBox(
+              width: 350,  // Set the width as per your requirement
+              height: 80, // Set the height as per your requirement
+              child: Card(
+                  color: Colors.grey,
+                  child: Padding(
+                      padding: EdgeInsets.all(8.5),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                                children: [
+                                  Row(children: [Text("Wed Mar 20"),Card(color: Colors.pink,child:Text('Today', ))]),
+                                  Row(children: [Text('3/-1 C    '), Icon(Icons.cloud_outlined)],)
+                                ]
+                            ),
+                            SizedBox( width:60, height:60,child: Card(child:Icon(Icons.edit_calendar)),),
+                          ]
+                      )
+                  )
+              )
+          )
+        ],
+      ),
+      Row(  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.black,
+            ),
+            onPressed: () {
+              // Handle button press
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: 1.0, color: Colors.black),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Closet'),
+              ),
+            ),
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.black,
+            ),
+            onPressed: () {
+              // Handle button press
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: 1.0, color: Colors.black),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Outfit'),
+              ),
+            ),
+          ),
+        ],),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox( height:190,width: 190,child: Row(children: [Column(children: [Image.network('https://shorturl.at/gyEJ5',height: 190/2,width: 190/2,),Image.network('https://shorturl.at/gyEJ5',height: 190/2,width: 190/2,)]),
+            Column(children: [Image.network('https://shorturl.at/gyEJ5',height: 190/2,width: 190/2,),Image.network('https://shorturl.at/gyEJ5',height: 190/2,width: 190/2,)])],),),
+          SizedBox(height:190,width: 190, child: Card(color: Colors.white,child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [Icon(Icons.door_sliding_outlined, size: 90.0,),Text('Create a closet')],),),)
+        ],
+      ),
+      Row(children: [Column(children: [Text('   All clothes'),Text('1')],),],),
+      Row(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: (){},
+            child: Row(children: [Icon(Icons.archive_outlined,color: Colors.black,),Text('Archive', style: TextStyle(color: Colors.black),),],),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+            ),
+          ),
+        ],
+      )
+
+    ]),
     Text(
       'Add',
       style: optionStyle,
